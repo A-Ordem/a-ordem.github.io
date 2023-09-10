aa21d66feee29d631ad891bbda8a3c7e6578c785
Gabriel Morais <gabriel.hmorais11@gmail.com>
2023-09-09 22:19:13 -0300

    um monte de bosta

---
+++ teste_varredura_brilho.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def plot_dynamic_brightness_curve(image_path):
+    # Carrega a imagem em cores
+    image = cv2.imread(image_path)
+
+    # Converte a imagem para o espaço de cores YUV
+    yuv_image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+
+    # Extrai a componente de luminância (Y)
+    y_channel = yuv_image[:, :, 0]
+
+    # Inicializa a figura com dois subplots ao lado
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    plt.subplots_adjust(wspace=0.3)  # Espaçamento entre os subplots
+
+    # Configuração inicial dos gráficos
+    ax1.set_title("Curva de Brilho Dinâmica (Vertical)")
+    ax1.set_xlabel("Comprimento Vertical")
+    ax1.set_ylabel("Brilho")
+    line, = ax1.plot([], [], lw=2)
+
+    ax2.set_title("Imagem")
+    im = ax2.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), aspect='auto', animated=True)
+    line_position = ax2.axhline(y=0, color='red', lw=2)  # Linha horizontal inicial
+
+    # Função de inicialização do gráfico
+    def init():
+        line.set_data([], [])
+        line_position.set_ydata(0)
+        return line, im, line_position
+
+    # Função de atualização do gráfico
+    def update(frame):
+        if frame < y_channel.shape[1]:
+            line.set_data(np.arange(0, y_channel.shape[0]), y_channel[:, frame])
+            line_position.set_ydata(frame)  # Atualiza a posição da linha vermelha
+            ax1.relim()
+            ax1.autoscale_view()
+        return line, im, line_position
+
+    # Criação da animação
+    interval = 100  # 0,1 segundo
+    frames = y_channel.shape[1]
+    ani = FuncAnimation(fig, update, frames=frames, init_func=init, blit=True, repeat=False, interval=interval)
+
+    # Exibe o gráfico
+    plt.show()
+
+# Substitua 'imagem.jpg' pelo caminho da sua imagem colorida
+image_path = 'imgtest_low.png'
+plot_dynamic_brightness_curve(image_path)
