f79c992044536d0b915d9b95d17ac25e1d71f469
Gabriel Morais <gabriel.hmorais11@gmail.com>
2023-09-09 23:23:00 -0300

    valor medio na varredura de brilho

--- teste_varredura_brilho.py
+++ teste_varredura_brilho.py
@@ -27,20 +27,31 @@ def plot_dynamic_brightness_curve(image_path):
     im = ax2.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), aspect='auto', animated=True)
     line_position = ax2.axhline(y=0, color='red', lw=2)  # Linha horizontal inicial
 
+    # Texto para exibir o valor médio de brilho
+    text = ax1.text(0.5, 0.95, '', transform=ax1.transAxes, ha='center', fontsize=12, color='red')
+
     # Função de inicialização do gráfico
     def init():
         line.set_data([], [])
         line_position.set_ydata(0)
-        return line, im, line_position
+        text.set_text('')
+        return line, im, line_position, text
 
     # Função de atualização do gráfico
     def update(frame):
         if frame < y_channel.shape[1]:
-            line.set_data(np.arange(0, y_channel.shape[0]), y_channel[:, frame])
+            line_data = y_channel[:, frame]
+            line.set_data(np.arange(0, y_channel.shape[0]), line_data)
             line_position.set_ydata(frame)  # Atualiza a posição da linha vermelha
+            
+            # Calcula o valor médio de brilho da linha atual
+            mean_brightness = np.mean(line_data)
+            text.set_text(f'Valor Médio de Brilho: {mean_brightness:.2f}')
+            
             ax1.relim()
             ax1.autoscale_view()
-        return line, im, line_position
+            
+        return line, im, line_position, text
 
     # Criação da animação
     interval = 100  # 0,1 segundo
@@ -51,5 +62,5 @@ def plot_dynamic_brightness_curve(image_path):
     plt.show()
 
 # Substitua 'imagem.jpg' pelo caminho da sua imagem colorida
-image_path = 'gradiente.jpg'
+image_path = 'imgtest_low.png'
 plot_dynamic_brightness_curve(image_path)
